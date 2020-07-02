6c2f350dbd2b4094a82573efeca5e06cf573e234
Daniel Cole <danielscole1@gmail.com>
2020-07-02 18:11:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,10 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  
+  array.each do |character|
+    puts "This #{character} wishes to bid you a good day."
+  end
 end
 
 def list_dwarves(array)
